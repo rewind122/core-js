ab268568aebebdd2eb2d9a1662cda252b1bd03c4
rewind122 <luvyoudear12@gmail.com>
2024-05-29 15:53:09 +0900

    0529 core js

---
+++ client/chapter/core/02.variable.js
@@ -0,0 +1,46 @@
+/* ------------------ */
+/* Variables          */
+/* ------------------ */
+
+let admin;
+let name;
+name = "John";
+admin = name;
+
+console.log(admin);
+
+let ourPlanet = "지구";
+let connectingUserName = "접속중인 유저";
+// currentUserName...
+
+
+/* 다음 내용을 분석한 후, 프로그래밍 할 수 있도록 변수와 상수를 작성해봅니다. ----------- */
+
+// - 갯수 별 상품 가격 계산하기
+let calcProductPriceQuentity = "갯수 별 상품 가격";
+// calculate = calc
+
+// - 구매 제품 가격의 총 합
+let totalProductPrice = "구매 제품 가격의 총 합";
+// - 1년 기준 일(day)자 수
+const DAY_PER_YEAR = "365";
+// - 구매 결제 여부
+let isPayment = false;
+let hasPayment = false;
+// 불린데이터 변수명 앞에는 관례적으로 is/has가 붙는다.
+// hasClass -> t/f
+
+// - 구매 결제 내역
+let PaymentList = "구매 결제 내역";
+// - 브랜드 접두사
+const BRAND_PREFIX = "브랜드 접두사";
+// - 오늘의 운세
+let fortuneToday = "오늘의 운세";
+// - 상품 정보
+const productInfo = "상품 정보";
+
+/* variables ----------------------------------------------------------- */
+
+/* constant variables -------------------------------------------------- */
+
+
