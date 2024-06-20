fbafa0931586b3e320edab937f76a722b392e325
rewind122 <luvyoudear12@gmail.com>
2024-06-20 10:44:26 +0900

    dom 스타벅스 gsap ver

--- client/starbucks/main.js
+++ client/starbucks/main.js
@@ -4,16 +4,13 @@ const header = document.querySelector('#header');
 
 const h = (t) => (t.style.height = 0);
 
+/* global gsap */
+
 aList.forEach((a) => {
-  a.addEventListener('mouseenter', () => {
-    const target = a.lastElementChild;
+  const target = a.lastElementChild;
 
-    // 모든 depth 높이를 0
-    depthList.forEach(h);
-    // 반환하는 값이 함수에 전달하는 인수와 동일하다면 함수를 생략해서 적는 게 가능?
+  const tl = gsap.timeline({ paused: true }).to(target, { height: 100, ease: 'power3.inOut' });
 
-    target.style.height = '100px';
-  });
+  a.addEventListener('mouseenter', () => tl.play());
+  a.addEventListener('mouseleave', () => tl.reverse());
 });
-
-header.addEventListener('mouseleave', () => depthList.forEach(h));
