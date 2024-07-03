1624933f91238a5aeb67403ea58d2d0c5a5c8f80
rewind122 <luvyoudear12@gmail.com>
2024-07-03 13:28:12 +0900

    webComponent - shadowDOM slot, styling

--- client/main.js
+++ client/main.js
@@ -1,14 +1,34 @@
+class UserCard extends HTMLElement{
+  constructor(){
+    super();
+    this.attachShadow({mode:'open'});
+    this.shadowRoot.innerHTML = `
+      <style>
+        :host{
+          background-color: orange;
+        }
+        div{
+          background:red;
+        }
+      </style>
+        <div> nickName : kind-tiger </div>
+        <slot name="username"></slot>
+        <slot name="age"></slot>
+        <slot name="gender"></slot>
+        <slot name="common"></slot>
+        <slot></slot>
+    `;
+  }
+}
 
-const template = document.createElement('template');
 
-template.innerHTML = `
-  <div>bye</div>
-  <div>javascript</div>
-`
+customElements.define('user-card', UserCard)
 
-const app = document.querySelector('#app');
-// const temp = document.querySelector('#temp');
 
-const clone = template.content.cloneNode(true);
 
-app.appendChild(clone)
+
+
+
+
+
+
