01080f3129717e44cff9e76d0c47d65c062b24b1
rewind122 <luvyoudear12@gmail.com>
2024-07-03 11:15:06 +0900

    webComponet - shadow DOM

--- client/main.js
+++ client/main.js
@@ -1,11 +1,32 @@
-class Button extends HTMLButtonElement {
-  constructor() {
+class Button extends HTMLElement {
+  constructor(){
     super();
+
+    // c-button의 쉐도우 돔을 열어줘.
+    // 캡슐화하려면 닫아줘야함
+    // 닫은 상태면 내부에서만 접근 가능
+    this.attachShadow({ mode: 'open' });
+    // this.attachShadow({ mode: 'closed' });
+
+    
+    // 그리고 그 안에(shadowRoot) 내가 원하는 태그 집어넣을거야.
+    this.shadowRoot.innerHTML = /* html */ `
+      <button>hello</button>
+    `   // open 상태일 때
+    // shadow.innerHTML = /* html */ `
+    //   <button>hello</button>
+    // `   // closed 상태일 때
   }
 
-  connectedCallback() {}
+  connectedCallback(){
 
-  disconnectedCallback() {}
+  }
+
+  disconnectedCallback(){
+
+  }
 }
 
-customElements.define('c-button', Button, {extends:'button'});
+customElements.define('c-button', Button);
+
+document.querySelector('c-button').shadowRoot.querySelector('button')
