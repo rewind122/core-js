0a0e8120bf9a055c62638e3252db35bc1dddad86
rewind122 <luvyoudear12@gmail.com>
2024-07-03 11:39:03 +0900

    webComponent - template

--- client/main.js
+++ client/main.js
@@ -1,32 +1,14 @@
-class Button extends HTMLElement {
-  constructor(){
-    super();
 
-    // c-button의 쉐도우 돔을 열어줘.
-    // 캡슐화하려면 닫아줘야함
-    // 닫은 상태면 내부에서만 접근 가능
-    this.attachShadow({ mode: 'open' });
-    // this.attachShadow({ mode: 'closed' });
+const template = document.createElement('template');
 
-    
-    // 그리고 그 안에(shadowRoot) 내가 원하는 태그 집어넣을거야.
-    this.shadowRoot.innerHTML = /* html */ `
-      <button>hello</button>
-    `   // open 상태일 때
-    // shadow.innerHTML = /* html */ `
-    //   <button>hello</button>
-    // `   // closed 상태일 때
-  }
+template.innerHTML = `
+  <div>bye</div>
+  <div>javascript</div>
+`
 
-  connectedCallback(){
+const app = document.querySelector('#app');
+// const temp = document.querySelector('#temp');
 
-  }
+const clone = template.content.cloneNode(true);
 
-  disconnectedCallback(){
-
-  }
-}
-
-customElements.define('c-button', Button);
-
-document.querySelector('c-button').shadowRoot.querySelector('button')
+app.appendChild(clone)
