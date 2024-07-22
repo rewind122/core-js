d906ce0db9b3991926d7414b7106e516ce53b027
rewind122 <luvyoudear12@gmail.com>
2024-07-22 13:03:53 +0900

    core-js

--- client/main.js
+++ client/main.js
@@ -1,52 +1,19 @@
+import { TodoList } from "./components/TodoList/TodoList.js";
+import { TodoItem } from "./components/TodoItem/TodoItem.js";
 
 
-class UserCard extends HTMLElement{
-  constructor(){
-    super();
-    this.attachShadow({mode:'open'});
-    this.shadowRoot.innerHTML = `
-      <button type="button">btn</button>
-    `
-    this.button = this.shadowRoot.querySelector('button');
-  }
+const app = document.getElementById('app');
 
-  connectedCallback(){
-
-    this.button.addEventListener('click', () => this.clickMe());
-    // arrow function은 자체적으로 this 를 가지지 않기 때문에 부모의 this를 가져옴
-    // this.button.addEventListener('click', this.clickMe.bind(this));
-    // bind -> 함수를 실행하지 않고 this 전달
-
-  }
-
-  
-
-  clickMe(){
-    console.log(this);
-  }
-
-
-
+const defineElements = () => {
+  customElements.define('todo-app', TodoList)
+  customElements.define('todo-item', TodoItem)
 }
 
-
-customElements.define('user-card',UserCard);
-
+defineElements();
 
 
-// console.log( document.querySelector('user-card').shadowRoot.querySelector('button') );
+const todoElement = document.createElement('todo-app');
+
+app.append(todoElement)
 
 
-
-
-// function sum(){
-//   console.log(this);
-// }
-
-
-// sum() // undefined
-// sum.call({}) // {} => 실행
-// sum.apply({}) // {} => 실행
-// const n = sum.bind({}) // {} => 실행 x 
-
-// n()
